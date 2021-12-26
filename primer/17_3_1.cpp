5fba4db8af356763f30649006e0a4126c152f0ba
PlasmolysisMango <mrgrandpa@163.com>
2021-12-26 16:05:39 +0800

    20211226

--- primer/17_3_1.cpp
+++ primer/17_3_1.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 #include <string>
 #include <regex>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
-    string pattern = "[[:alnum:]]*[^c]ei[[:alnum:]]*$";
-    string str = "ceishi height";
-    smatch result;
-    regex r(pattern);
-    if (regex_search(str, result, r))
-        cout << result.str();
+    // string pattern = "[[:alnum:]]*[^c]ei[[:alnum:]]*$";
+    // string str = "ceishi height";
+    // smatch result;
+    // regex r(pattern);
+    // if (regex_search(str, result, r))
+    //     cout << result.str();
+    string s;
+    vector<string> s;
 }
